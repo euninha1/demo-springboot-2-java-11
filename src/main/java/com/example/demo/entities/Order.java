9c43fd8af0fe61a17b3895d48d228e8c0d90b273
Fernanda Amaral <fernanda.amaral00@souunit.com.br>
2020-06-05 16:07:57 -0300

    OrderStatus enum

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -11,20 +11,23 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.example.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-@Entity // informando que a classe também é uma entidade
-@Table(name = "tb_order") // alterando o nome da tabela
+@Entity 						// informando que a classe também é uma entidade
+@Table(name = "tb_order") 							// alterando o nome da tabela
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id // dizendo que é uma tabela no banco de dados
+	@Id 									// dizendo que é uma tabela no banco de dados
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-
+	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id") // esta coluna esta na tabela User
 	private User client;
@@ -33,10 +36,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -56,6 +60,15 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus !=null) {
+		this.orderStatus = orderStatus.getCode();
+		}
+	}
 	public User getClient() {
 		return client;
 	}
